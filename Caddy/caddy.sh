84a0ce00acf6e3d2f3ab6c9e44516386296d26dd
NicolasG <37571306+GouGoGoal@users.noreply.github.com>
2019-08-26 12:52:59 +0800

    Update caddy.sh

--- Caddy/caddy.sh
+++ Caddy/caddy.sh
@@ -1,15 +1,28 @@
 apt update
 apt install -y curl
+
 curl https://getcaddy.com | bash -s personal
+
 mkdir /etc/caddy
 touch /etc/caddy/Caddyfile
 chown -R root:www-data /etc/caddy
+
 mkdir /etc/caddy/ssl
 chown -R www-data:root /etc/caddy/ssl
 chmod 0770 /etc/caddy/ssl
+
 mkdir /etc/caddy/www
 chown www-data:www-data /etc/caddy/www
-curl -s  https://raw.githubusercontent.com/gougogoal/v2ray/master/caddy.service  -o /etc/systemd/system/caddy.service
-systemctl enable caddy
+
+curl -s https://raw.githubusercontent.com/gougogoal/v2ray/master/caddy.service  -o /etc/systemd/system/caddy.service
 curl -s https://raw.githubusercontent.com/gougogoal/v2ray/master/Caddyfile -o /etc/caddy/Caddyfile
-vi /etc/caddy/Caddyfile
+wget --no-check-certificate -P /etc/caddy "https://raw.githubusercontent.com/GouGoGoal/v2ray/master/Caddy/ssl.sh"
+
+systemctl enable caddy
+
+chmod 755 /etc/caddy/ssl.sh
+bash /etc/caddy/ssl.sh
+echo "4 0 0 0 1 root /etc/caddy/ssl.sh">>/etc/crontab
+systemctl status caddy
+
+
