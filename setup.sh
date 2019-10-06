fe73ab2e978833fa408b9421a51dca2a9044bbf9
NicolasG <37571306+GouGoGoal@users.noreply.github.com>
2019-10-06 16:00:59 +0800

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+apt update
+apt install vim curl -y
+cd /root
+git clone -b master https://github.com/GouGoGoal/v2ray
+cd v2ray
+sed -i "s/id_value/$1/g" config.json
+mv v2ray.service /etc/systemd/system/
+systemctl enable v2ray
+systemctl restart v2ray
+
