a9d9111b90c624d2c57407b8d3a85807e06fe027
NicolasG <37571306+GouGoGoal@users.noreply.github.com>
2019-12-13 20:42:09 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ git clone -b master https://github.com/GouGoGoal/v2ray
 cd v2ray
 chmod 755 /root/v2ray/*
 sed -i "s/id_value/$1/g" config.json
-mv v2ray.service /etc/systemd/system/
+cp v2ray.service /etc/systemd/system/
 systemctl enable v2ray
 systemctl restart v2ray
 bash /root/v2ray/Caddy/caddy.sh
