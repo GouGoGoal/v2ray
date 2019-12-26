31eeff73fa64320419ce336485742975bf342e33
NicolasG <37571306+GouGoGoal@users.noreply.github.com>
2019-12-26 18:50:41 +0800

    Update ban.sh

--- ban.sh
+++ ban.sh
@@ -2,9 +2,10 @@
 
 iptables -F OUTPUT
 
-#屏蔽敏感域名
+#政府地址
 iptables -A OUTPUT -m string --string "gov.cn" --algo bm --to 65535 -j DROP
 iptables -A OUTPUT -m string --string "12377.cn" --algo bm --to 65535 -j DROP
+#法*功
 iptables -A OUTPUT -m string --string "falunaz" --algo bm --to 65535 -j DROP
 iptables -A OUTPUT -m string --string "falundafa" --algo bm --to 65535 -j DROP
 iptables -A OUTPUT -m string --string "minghui" --algo bm --to 65535 -j DROP
@@ -13,6 +14,12 @@ iptables -A OUTPUT -m string --string "dongtaiwang" --algo bm --to 65535 -j DROP
 iptables -A OUTPUT -m string --string "wujieliulan" --algo bm --to 65535 -j DROP
 iptables -A OUTPUT -m string --string "mhradio" --algo bm --to 65535 -j DROP
 iptables -A OUTPUT -m string --string "ntdtv" --algo bm --to 65535 -j DROP
+#360和百度
+iptables -A OUTPUT -m string --string "360.c" --algo bm --to 65535 -j DROP
+iptables -A OUTPUT -m string --string "so.c" --algo bm --to 65535 -j DROP
+iptables -A OUTPUT -m string --string "api.baidu.com" --algo bm --to 65535 -j DROP
+iptables -A OUTPUT -m string --string "shifen.com" --algo bm --to 65535 -j DROP
+iptables -A OUTPUT -m string --string "baidu.com" --algo bm --to 65535 -j DROP
 
 
 #拉取规则
