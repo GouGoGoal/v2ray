8addfff6535f228bf5c77796a6b2be2460a523b7
NicolasG <37571306+GouGoGoal@users.noreply.github.com>
2019-12-26 18:18:25 +0800

    Update ban.sh

--- ban.sh
+++ ban.sh
@@ -1,5 +1,7 @@
 #! /bin/sh
 
+iptables -F OUTPUT
+
 #屏蔽部分法轮功域名
 iptables -A OUTPUT -m string --string "gov.cn" --algo bm --to 65535 -j DROP
 iptables -A OUTPUT -m string --string "12377.cn" --algo bm --to 65535 -j DROP
@@ -11,8 +13,7 @@ iptables -A OUTPUT -m string --string "dongtaiwang.com" --algo bm --to 65535 -j 
 iptables -A OUTPUT -m string --string "wujieliulan.com" --algo bm --to 65535 -j DROP
 iptables -A OUTPUT -m string --string "mhradio.org" --algo bm --to 65535 -j DROP
 iptables -A OUTPUT -m string --string "ntdtv.com" --algo bm --to 65535 -j DROP
-iptables -A OUTPUT -m string --string "法轮功" --algo bm --to 65535 -j DROP
-iptables -A OUTPUT -m string --string "反共" --algo bm --to 65535 -j DROP
-iptables -A OUTPUT -m string --string "台毒" --algo bm --to 65535 -j DROP
 
+
+#拉取规则
 wget -N --no-check-certificate  -P /root/v2ray "https://raw.githubusercontent.com/GouGoGoal/v2ray/master/ban.sh"
