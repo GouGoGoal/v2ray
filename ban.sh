2945120bfd5d164fce6874ef4fcf9a93d63de628
NicolasG <37571306+GouGoGoal@users.noreply.github.com>
2019-12-26 18:29:04 +0800

    Update ban.sh

--- ban.sh
+++ ban.sh
@@ -5,14 +5,14 @@ iptables -F OUTPUT
 #屏蔽敏感域名
 iptables -A OUTPUT -m string --string "gov.cn" --algo bm --to 65535 -j DROP
 iptables -A OUTPUT -m string --string "12377.cn" --algo bm --to 65535 -j DROP
-iptables -A OUTPUT -m string --string "falunaz.net" --algo bm --to 65535 -j DROP
-iptables -A OUTPUT -m string --string "falundafa.org" --algo bm --to 65535 -j DROP
-iptables -A OUTPUT -m string --string "minghui.org" --algo bm --to 65535 -j DROP
-iptables -A OUTPUT -m string --string "epochtimes.com" --algo bm --to 65535 -j DROP
-iptables -A OUTPUT -m string --string "dongtaiwang.com" --algo bm --to 65535 -j DROP
-iptables -A OUTPUT -m string --string "wujieliulan.com" --algo bm --to 65535 -j DROP
-iptables -A OUTPUT -m string --string "mhradio.org" --algo bm --to 65535 -j DROP
-iptables -A OUTPUT -m string --string "ntdtv.com" --algo bm --to 65535 -j DROP
+iptables -A OUTPUT -m string --string "falunaz" --algo bm --to 65535 -j DROP
+iptables -A OUTPUT -m string --string "falundafa" --algo bm --to 65535 -j DROP
+iptables -A OUTPUT -m string --string "minghui" --algo bm --to 65535 -j DROP
+iptables -A OUTPUT -m string --string "epochtimes" --algo bm --to 65535 -j DROP
+iptables -A OUTPUT -m string --string "dongtaiwang" --algo bm --to 65535 -j DROP
+iptables -A OUTPUT -m string --string "wujieliulan" --algo bm --to 65535 -j DROP
+iptables -A OUTPUT -m string --string "mhradio" --algo bm --to 65535 -j DROP
+iptables -A OUTPUT -m string --string "ntdtv" --algo bm --to 65535 -j DROP
 
 
 #拉取规则
