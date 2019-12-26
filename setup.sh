3ddc313d7bb160274cf32cbe63a465bbf1d80116
NicolasG <37571306+GouGoGoal@users.noreply.github.com>
2019-12-26 14:35:48 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,5 +10,6 @@ sed -i "s/id_value/$1/g" config.json
 cp v2ray.service /etc/systemd/system/
 systemctl enable v2ray
 systemctl restart v2ray
+rm -rf  README.md setup.sh 常用配置 .git/
 bash /root/v2ray/Caddy/caddy.sh
 
