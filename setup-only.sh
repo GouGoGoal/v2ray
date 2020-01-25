1db45bd3d8ad9321c61a53e447e654e46a21fa1e
NicolasG <37571306+GouGoGoal@users.noreply.github.com>
2020-01-25 18:47:25 +0800

    Update setup-only.sh

--- setup-only.sh
+++ setup-only.sh
@@ -6,7 +6,6 @@ apt install git -y
 cd /root
 git clone -b master https://github.com/GouGoGoal/v2ray
 cd v2ray
-rm -rf  README.md setup.sh 常用配置 .git
 chmod 755 /root/v2ray/*
 
 #更改对接nodeid
@@ -27,13 +26,100 @@ echo "
 #每周一删除日志
 29 2 * * 1 root rm -rf /var/log/*.gz ">>/etc/crontab
 
-
-
 #开机自启服务
-cp v2ray.service /etc/systemd/system/
+mv v2ray.service /etc/systemd/system/
 systemctl enable v2ray
 systemctl restart v2ray
+#删除多余文件
+rm -rf  README.md setup*.sh 常用配置 .git* 
+
+read -s -n1 -p "是否优化系统参数"
+#优化最大文件打开
+echo "
+root soft nofile 512000
+root hard nofile 512000
+">>/etc/security/limits.conf
+#BBR以及内核优化
+echo "
+#关闭IPV6
+net.ipv6.conf.all.disable_ipv6 = 1
+#开启BBR
+net.core.default_qdisc=fq
+net.ipv4.tcp_congestion_control=bbr
+#开启内核转发
+net.ipv4.ip_forward=1
+#优先使用ram
+vm.swappiness=0
+#TCP优化
+fs.file-max = 512000
+net.core.rmem_max = 67108864
+net.core.wmem_max = 67108864
+net.core.netdev_max_backlog = 250000
+net.core.somaxconn = 4096
+net.ipv4.tcp_syncookies = 1
+net.ipv4.tcp_tw_reuse = 1
+net.ipv4.tcp_fin_timeout = 30
+net.ipv4.tcp_keepalive_time = 1200
+net.ipv4.ip_local_port_range = 10000 65000
+net.ipv4.tcp_max_syn_backlog = 8192
+net.ipv4.tcp_max_tw_buckets = 5000
+net.ipv4.tcp_fastopen = 3
+net.ipv4.tcp_mem = 25600 51200 102400
+net.ipv4.tcp_rmem = 4096 87380 67108864
+net.ipv4.tcp_wmem = 4096 65536 67108864
+net.ipv4.tcp_mtu_probing = 1
+">/etc/sysctl.conf
+sysctl -p
+read -s -n1 -p "更改开机等待时间，仅KVM"
+#更改开机启动时间1S
+sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=1/' /etc/default/grub
+update-grub
+###########
+
 #安装caddy
 bash /root/v2ray/Caddy/caddy.sh
+
+read -s -n1 -p "是否优化系统参数"
+#优化最大文件打开
+echo "
+root soft nofile 512000
+root hard nofile 512000
+">>/etc/security/limits.conf
+#BBR以及内核优化
+echo "
+#关闭IPV6
+net.ipv6.conf.all.disable_ipv6 = 1
+#开启BBR
+net.core.default_qdisc=fq
+net.ipv4.tcp_congestion_control=bbr
+#开启内核转发
+net.ipv4.ip_forward=1
+#优先使用ram
+vm.swappiness=0
+#TCP优化
+fs.file-max = 512000
+net.core.rmem_max = 67108864
+net.core.wmem_max = 67108864
+net.core.netdev_max_backlog = 250000
+net.core.somaxconn = 4096
+net.ipv4.tcp_syncookies = 1
+net.ipv4.tcp_tw_reuse = 1
+net.ipv4.tcp_fin_timeout = 30
+net.ipv4.tcp_keepalive_time = 1200
+net.ipv4.ip_local_port_range = 10000 65000
+net.ipv4.tcp_max_syn_backlog = 8192
+net.ipv4.tcp_max_tw_buckets = 5000
+net.ipv4.tcp_fastopen = 3
+net.ipv4.tcp_mem = 25600 51200 102400
+net.ipv4.tcp_rmem = 4096 87380 67108864
+net.ipv4.tcp_wmem = 4096 65536 67108864
+net.ipv4.tcp_mtu_probing = 1
+">/etc/sysctl.conf
+sysctl -p
+read -s -n1 -p "更改开机等待时间，仅KVM"
+#更改开机启动时间1S
+sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=1/' /etc/default/grub
+update-grub
+###########
 
 
