d6178df549600142864b99fc91595faacba0ff1c
NicolasG <37571306+GouGoGoal@users.noreply.github.com>
2020-03-09 09:28:07 +0800

    Update caddy.sh

--- Caddy/caddy.sh
+++ Caddy/caddy.sh
@@ -3,7 +3,6 @@ cd /root/v2ray/Caddy
 chmod 755 *
 mv -f  caddy.service  /etc/systemd/system/caddy.service
 
-bash /etc/caddy/ssl.sh
 echo "10 3 * * 1 root /root/v2ray/Caddy/tls_auto.sh">>/etc/crontab
 rm -rf caddy.sh ssl.sh
 systemctl enable caddy
