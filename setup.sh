049d136c2d76f030b609bff5b000310ea8979d07
NicolasG <37571306+GouGoGoal@users.noreply.github.com>
2020-04-26 14:56:15 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,12 +11,16 @@ rm -rf  README.md setup.sh 常用配置 .git*
 #更改对接nodeid
 sed -i "s/id_value/$1/g" config.json
 
-#添加开机脚本，屏蔽域名
-echo '#!/bin/sh -e
-bash /root/v2ray/ban.sh
-exit 0' >/etc/rc.local
-chmod 755 /etc/rc.local
-systemctl restart rc.local
+#更改开机自启
+if [ ! -f "/etc/rc.local" ]; then
+    echo '#!/bin/sh -e
+    bash /root/v2ray/ban.sh
+    exit 0' >/etc/rc.local
+    chmod +x /etc/rc.local
+    systemctl restart rc.local
+else 
+    sed -i '$i\bash /root/v2ray/ban.sh' /etc/rc.local
+fi
 
 #开机自启服务
 mv -f v2ray.service /etc/systemd/system/
