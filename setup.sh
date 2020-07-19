159cbf2797976ac8387ba60f7925ec4017fb0026
NicolasG <37571306+GouGoGoal@users.noreply.github.com>
2020-07-19 15:55:41 +0800

    1

--- setup.sh
+++ setup.sh
@@ -13,21 +13,6 @@ chmod +x v2ray v2ctl *.sh
 rm -rf  README.md setup.sh 常用配置 .git*
 #更改对接nodeid
 sed -i "s/id_value/$1/g" config.json
-
-#添加iptables屏蔽
-if [ ! -f "/etc/rc.local" ]; then
-    if [ -f "/etc/rc.d/rc.local" ]; then
-        ln -s /etc/rc.d/rc.local /etc/rc.local
-    fi
-    echo '#!/bin/sh -e
-bash /root/v2ray/ban.sh
-exit 0' >/etc/rc.local
-    chmod +x /etc/rc.local
-    systemctl restart rc-local
-else 
-    sed -i '$i\bash /root/v2ray/ban.sh' /etc/rc.local
-fi
-
 #开机自启服务
 mv -f v2ray.service /etc/systemd/system/
 systemctl enable v2ray
