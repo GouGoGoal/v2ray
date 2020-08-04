b199ad00925cfe60d59607be04e45a05b0ac67cc
NicolasG <37571306+GouGoGoal@users.noreply.github.com>
2020-08-04 21:47:02 +0800

    Update caddy.sh

--- Caddy/caddy.sh
+++ Caddy/caddy.sh
@@ -4,7 +4,7 @@ mv caddy /usr/bin
 mv caddy.service  /etc/systemd/system/caddy.service
 mkdir /etc/caddy && mv *  /etc/caddy
 
-echo "10 3 * * 1 root wget -N --no-check-certificate  -P /etc/caddy/ https://raw.githubusercontent.com/GouGoGoal/v2ray/master/Caddy/caddy_load.pem ">>/etc/crontab
+echo "50 5 * * 1 root wget -N --no-check-certificate  -P /etc/caddy/ https://raw.githubusercontent.com/GouGoGoal/v2ray/master/Caddy/caddy_load.pem ">>/etc/crontab
 rm -rf /etc/caddy/caddy.sh /etc/caddy/ssl.sh
 
 systemctl enable caddy
