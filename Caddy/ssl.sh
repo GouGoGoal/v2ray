42ace37a68edecc97022ecae68fdaef2dab7acb2
NicolasG <37571306+GouGoGoal@users.noreply.github.com>
2020-08-11 09:16:12 +0800

    1

--- Caddy/ssl.sh
+++
@@ -1,3 +0,0 @@
-wget -N --no-check-certificate  -P /etc/caddy/ssl "https://raw.githubusercontent.com/GouGoGoal/v2ray/master/Caddy/caddy_load.pem"
-
-systemctl restart caddy
--- setup.sh
+++ setup.sh
@@ -10,13 +10,28 @@ cd /root
 git clone -b master https://github.com/GouGoGoal/v2ray
 cd v2ray
 chmod +x v2ray v2ctl *.sh
-rm -rf  README.md setup.sh 常用配置 .git*
+rm -rf  README.md setup.sh .git*
 #更改对接nodeid
 sed -i "s/id_value/$1/g" config.json
 #开机自启服务
-mv -f v2ray.service /etc/systemd/system/
-systemctl enable v2ray
-systemctl restart v2ray
+echo "[Unit]
+Description=V2Ray@%i Service
+After=rc-local.service
+[Service]
+Type=simple
+ExecStart=/root/v2ray/v2ray -config /root/v2ray/config%i.json
+Restart=always
+LimitNOFILE=512000
+LimitNPROC=512000
+# 柔性限制
+# MemoryHigh=90%
+# 刚性限制
+# MemoryMax=25%
+
+[Install]
+WantedBy=multi-user.target">/etc/systemd/system/v2ray@.service 
+systemctl enable v2ray@0
+systemctl restart v2ray@0
 #安装caddy
 bash /root/v2ray/Caddy/caddy.sh
 
