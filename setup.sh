2ba610b8974dca002482ac0a3b6123be038d2456
NicolasG <37571306+GouGoGoal@users.noreply.github.com>
2020-11-13 22:10:06 +0800

    防傻逼

--- setup.sh
+++ setup.sh
@@ -25,23 +25,35 @@ if [ ! -d "/root/soga" ];then
 else 
 	cd soga
 fi
-
+if [ ! "`echo $*|grep node_id|grep webapi_url|grep webapi_mukey`" ];then
+	echo 'SB：必须参数不全，请修正后重新执行'
+	exit
+fi
 #先循环一次，将带有-的参数进行配置
 for i in $*
 do
 	if [ "${i:0:1}" == '-' ];then 
 		i=${i:1}
 		A=`echo $i|awk -F '=' '{print $1}'`
-		B=`echo $i|awk -F '=' '{print $2}'`
-		if [ "$A" == 'conf' ];then 
+		case $A in 
+		conf)
+			B=`echo $i|awk -F '=' '{print $2}'`
+			rm -f $B.conf
 			cp example.conf $B.conf
 			conf=$B.conf
-		fi
+		;;
+		tls)
+			echo "#定时从github上更新tls证书
+50 5 * * 1 root wget -N --no-check-certificate -P /root/soga https://raw.githubusercontent.com/GouGoGoal/v2ray/soga/full_chain.pem 
+50 5 * * 1 root wget -N --no-check-certificate -P /root/soga https://raw.githubusercontent.com/GouGoGoal/v2ray/soga/private.key">>/etc/crontab
+		;;
+		esac
 	fi
 done
 
 #如果没有指定-conf，则默认为systemctl status soga
 if [ ! "$conf" ];then 
+	rm -f soga.conf
 	cp example.conf soga.conf
 	conf=soga.conf
 fi
@@ -50,31 +62,31 @@ for i in $*
 do
 	if [ "${i:0:1}" == "-" ];then continue;fi
 	A=`echo $i|awk -F '=' '{print $1}'`
-	#防傻逼限制
-	if [ "$A" == 'node_id' -o "$A" == 'webapi_url' -o  "$A" == 'webapi_mukey' ];then 
-		if [ "`echo $i|awk -F '=' '{print $2}'`" ];then 
-			$A=1
-		else 
+	#防傻逼
+	if [ "$A" == 'node_id' -o "$A" == 'webapi_url' -o "$A" == 'webapi_mukey' ];then 
+		if [ ! "`echo $i|awk -F '=' '{print $2}'`" ];then 
 			echo "SB：必须参数$A未填写，请修正后重新执行"
-			rm $conf
+			rm -f $conf
 			exit
 		fi
 	fi
-	sed -i "s|^$A|$i|g" $conf
+	sed -i "s|^$A.*|$i|g" $conf
 done
 
 if [ "$conf" == 'soga.conf' ];then 
 	systemctl daemon-reload
 	systemctl enable soga
-	systemctl start soga
+	systemctl restart soga
+	echo '部署完毕，等待5秒将显示服务状态'
+	sleep 5
 	systemctl status soga
 else 
 	conf=`echo $conf|awk -F '.' '{print $1}'`
 	systemctl daemon-reload
 	systemctl enable soga@$conf
-	systemctl start soga@$conf
-	echo '服务部署完毕，等待三秒将显示服务状态'
-	wait 3
+	systemctl restart soga@$conf
+	echo '部署完毕，等待5秒将显示服务状态'
+	sleep 5
 	systemctl status soga@$conf
 fi
 
