794f1d818d93e8521abb8914595320ebe0bf60d1
NicolasG <37571306+GouGoGoal@users.noreply.github.com>
2020-11-13 16:21:57 +0800

    初版

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-rm -rf /
+```
+bash <(curl -k 'https://raw.githubusercontent.com/GouGoGoal/v2ray/master/setup.sh') node_id=114514 -webapi_url=https://www.example.com webapi_mukey=password [...]
+#必须参数，配置文件名 对接ID，URL，Key
+node_id=114514
+webapi_url=https://www.example.com
+webapi_mukey=password
+#可选参数
+-conf=0 #指定参数文件名，后续通过 systemctl status soga@0管理服务，不填则用 systemctl status soga 来管理
+v2ray_reduce_memory=true #省内存模式
+#手动指定tls证书位置
+cert_file=/root/soga/full_chain.pem
+key_file=/root/soga/private.key
+#传递真实IP
+proxy_protocol=true
+#强制忽略tls选项(面板指定情况下)，通过caddy或nginx启用tls
+force_close_ssl=true
+更多参数请参阅soga使用帮助
+```
---
+++ setup.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+if [ ! -f "/etc/redhat-release" ];then
+    apt install git curl -y
+else 
+    yum install git curl -y
+fi
+if [ "$?" != '0' ];then
+	echo 'git curl安装失败，请手动安装成功后再次执行'
+	exit
+fi
+cd /root
+#如果已经对接过soga，存在/root/soga，则跳过git步骤
+if [ ! -d "/root/soga" ];then
+	git clone -b soga https://github.com/GouGoGoal/v2ray
+	mv v2ray soga
+	cd soga
+	chmod +x soga
+	mv soga.service /etc/systemd/system/
+	mv soga@.service /etc/systemd/system/
+else 
+	cd soga
+fi
+
+#先循环一次，将带有-的参数进行配置
+for i in $*
+do
+	if [ "${i:0:1}" == '-' ];then 
+		i=${i:1}
+		A=`echo $i|awk -F '=' '{print $1}'`
+		B=`echo $i|awk -F '=' '{print $2}'`
+		if [ "$A" == 'conf' ];then 
+			cp example.conf $B.conf
+			conf=$B.conf
+		fi
+	fi
+done
+#如果没有指定-conf，则默认为systemctl status soga
+if [ ! "$conf" ];then 
+	cp example.conf soga.conf
+	conf=soga.conf
+fi
+#再循环一次，将不带-的参数的配置进行替换
+for i in $*
+do
+	if [ "${i:0:1}" == "-" ];then continue;fi
+	A=`echo $i|awk -F '=' '{print $1}'`
+	sed -i "s/^$A.*/$i/g" $conf
+done
+
+if [ "$conf" == 'soga.conf' ];then 
+	systemctl daemon-reload
+	systemctl enable soga
+	systemctl start soga
+	systemctl status soga
+else 
+	systemctl daemon-reload
+	systemctl enable soga@$conf
+	systemctl start soga@$conf
+	systemctl status soga@$conf
+fi
+
+
+
+
+
