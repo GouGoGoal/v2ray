c5ed647b978198e4d1d86eb0c189aa26f799746a
NicolasG <37571306+GouGoGoal@users.noreply.github.com>
2020-11-16 22:26:36 +0800

    测试开启ECN内核参数

--- setup.sh
+++ setup.sh
@@ -72,6 +72,7 @@ net.ipv4.tcp_window_scaling = 1
 net.ipv4.tcp_mtu_probing = 1
 net.ipv4.tcp_rmem = 4096 87380 67108864
 net.ipv4.tcp_wmem = 4096 65536 67108864
+net.ipv4.tcp_ecn = 1
 ">/etc/sysctl.conf
 			sysctl -p
 			;;
