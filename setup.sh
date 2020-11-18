889391d8a94109d2b13d54f74b1fe9fe96ac06ea
NicolasG <37571306+GouGoGoal@users.noreply.github.com>
2020-11-18 12:52:25 +0800

    内核参数调优

--- setup.sh
+++ setup.sh
@@ -62,7 +62,7 @@ net.ipv4.tcp_syncookies = 1
 net.ipv4.tcp_slow_start_after_idle=0
 net.ipv4.tcp_tw_reuse = 1
 net.ipv4.tcp_fin_timeout = 30
-net.ipv4.tcp_keepalive_time = 1200
+net.ipv4.tcp_keepalive_time = 120
 net.ipv4.ip_local_port_range = 10000 65000
 net.ipv4.tcp_max_syn_backlog = 8192
 net.ipv4.tcp_max_tw_buckets = 5000
@@ -70,8 +70,9 @@ net.ipv4.tcp_fastopen = 3
 net.ipv4.tcp_sack = 1
 net.ipv4.tcp_window_scaling = 1
 net.ipv4.tcp_mtu_probing = 1
-net.ipv4.tcp_rmem = 4096 87380 67108864
-net.ipv4.tcp_wmem = 4096 65536 67108864
+net.ipv4.tcp_rmem = 4096 131072 16777216
+net.ipv4.tcp_wmem = 4096 131072 16777216
+net.ipv4.tcp_mem = 4096 131072 16777216
 net.ipv4.tcp_ecn = 1
 ">/etc/sysctl.conf
 			sysctl -p
