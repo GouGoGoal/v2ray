da2c37a3ea943139e14c4f255fbd325315d2bb71
NicolasG <37571306+GouGoGoal@users.noreply.github.com>
2020-11-18 16:45:26 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -40,42 +40,7 @@ do
 		A=`echo $i|awk -F '=' '{print $1}'`
 		case $A in 
 		bbr)
-			echo "
-#关闭IPV6
-net.ipv6.conf.all.disable_ipv6 = 1
-#开启BBR
-net.core.default_qdisc=fq
-net.ipv4.tcp_congestion_control=bbr
-#开启内核转发
-net.ipv4.ip_forward=1
-#优先使用ram
-vm.swappiness=0
-#可以分配所有物理内存
-vm.overcommit_memory=1
-#TCP优化
-fs.file-max = 512000
-net.core.rmem_max = 67108864
-net.core.wmem_max = 67108864
-net.core.netdev_max_backlog = 250000
-net.core.somaxconn = 4096
-net.ipv4.tcp_syncookies = 1
-net.ipv4.tcp_slow_start_after_idle=0
-net.ipv4.tcp_tw_reuse = 1
-net.ipv4.tcp_fin_timeout = 30
-net.ipv4.tcp_keepalive_time = 120
-net.ipv4.ip_local_port_range = 10000 65000
-net.ipv4.tcp_max_syn_backlog = 8192
-net.ipv4.tcp_max_tw_buckets = 5000
-net.ipv4.tcp_fastopen = 3
-net.ipv4.tcp_sack = 1
-net.ipv4.tcp_window_scaling = 1
-net.ipv4.tcp_mtu_probing = 1
-net.ipv4.tcp_rmem = 4096 131072 16777216
-net.ipv4.tcp_wmem = 4096 131072 16777216
-net.ipv4.tcp_mem = 4096 131072 16777216
-net.ipv4.tcp_ecn = 1
-">/etc/sysctl.conf
-			sysctl -p
+			curl 'https://raw.githubusercontent.com/GouGoGoal/SHELL/master/sysctl.sh'|bash
 			;;
 		conf)
 			B=`echo $i|awk -F '=' '{print $2}'`
