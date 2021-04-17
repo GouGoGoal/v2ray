c0d9255b99f563e0c9eafbccd077bc5309417272
NicolasG <37571306+GouGoGoal@users.noreply.github.com>
2021-04-17 13:10:30 +0800

    Update task.sh

--- task.sh
+++ task.sh
@@ -5,6 +5,7 @@
 wget https://github.com/GouGoGoal/v2ray/raw/soga/key.pem -O /root/soga/key.pem
 wget https://github.com/GouGoGoal/v2ray/raw/soga/cert.pem -O /root/soga/cert.pem
 
+cd /root/soga
 for i in `ls |grep conf` 
 do 
 	sed -i "s|^cert_file.*|cert_file=/root/soga/cert.pem|g"  $i
