3755f3af795c14774934d49428e6a85edeb40468
NicolasG <37571306+GouGoGoal@users.noreply.github.com>
2021-04-18 12:40:52 +0800

    Update task.sh

--- task.sh
+++ task.sh
@@ -2,15 +2,5 @@
 
 
 
-wget https://github.com/GouGoGoal/v2ray/raw/soga/key.pem -O /root/soga/key.pem
-wget https://github.com/GouGoGoal/v2ray/raw/soga/cert.pem -O /root/soga/cert.pem
-
-cd /root/soga
-for i in `ls |grep conf` 
-do 
-	sed -i "s|^cert_file.*|cert_file=/root/soga/cert.pem|g"  $i
-	sed -i "s|^key_file.*|key_file=/root/soga/key.pem|g"   $i
-done
-
-
-systemctl restart soga@xtls0 soga@xtls1
+wget  https://github.com/GouGoGoal/v2ray/raw/soga/ban.conf -O /etc/ban.conf
+systemctl restart smartdns
