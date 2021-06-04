088bdc8d394c10b63350c32e7f8b88f144162512
NicolasG <37571306+GouGoGoal@users.noreply.github.com>
2021-06-04 16:53:37 +0800

    Update task.sh

--- task.sh
+++ task.sh
@@ -1,7 +1,1 @@
 #!/bin/bash
-
-sed -i '/.*root for i in.*/d' /etc/crontab
-sed -i '/.*重启服务/d' /etc/crontab
-
-echo '#每天06:00点重启服务'>>/etc/crontab
-echo "0 6 * * * root for i in \`systemctl |grep -E \"soga|nginx|state|sniproxy\"|grep service|awk '{print \$1}'\`;do systemctl restart \$i;done" >>/etc/crontab
